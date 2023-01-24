5dd419d581b66da36d3da1ea689e189a94c3a35e
[super1Nova] <super1sun@naver.com>
2023-01-24 23:29:22 +0900

    spring bean 생명주기

---
+++ 5_BeanLifeCycle/src/main/java/kr/co/softcampus/beans/TestBean1.java
@@ -0,0 +1,15 @@
+package kr.co.softcampus.beans;
+
+public class TestBean1 {
+	public TestBean1() {
+		System.out.println("TestBean1의 생성자 호출");
+	}
+	
+	public void bean1_init(){
+		System.out.println("TestBean1의 init 메서드");
+	}
+	
+	public void bean1_destroy() {
+		System.out.println("TestBean1의 destroy 메서드");
+	}
+}
---
+++ 5_BeanLifeCycle/src/main/java/kr/co/softcampus/beans/TestBean4.java
@@ -0,0 +1,7 @@
+package kr.co.softcampus.beans;
+
+public class TestBean4 {
+	public TestBean4() {
+		System.out.println("TestBean4 생성자 호출");
+	}
+}
