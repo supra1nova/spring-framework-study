a91f554d1b00a284e8b33c671f2e60d341e9e2dd
[super1Nova] <super1sun@naver.com>
2023-01-24 22:02:46 +0900

    java 그리고 polymorphism

---
+++ 1_javaBasic/src/kr/co/softcampus/beans/HelloWorldKo.java
@@ -0,0 +1,9 @@
+package kr.co.softcampus.beans;
+
+public class HelloWorldKo {
+	
+	public void sayHello() {
+		System.out.println("안녕하세요~~");
+	}
+
+}
---
+++ 1_javaBasic/src/kr/co/softcampus/main/MainClass.java
@@ -0,0 +1,30 @@
+package kr.co.softcampus.main;
+
+import kr.co.softcampus.beans.HelloWorldEn;
+import kr.co.softcampus.beans.HelloWorldKo;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+//		HelloWorldEn hello1 = new HelloWorldEn();
+//		callMethod(hello1);
+//		
+//		HelloWorldEn hello2 = new HelloWorldEn();
+//		callMethod(hello2);
+		
+		HelloWorldKo hello1 = new HelloWorldKo();
+		callMethod(hello1);
+		
+		HelloWorldKo hello2 = new HelloWorldKo();
+		callMethod(hello2);
+	}
+	
+//	public static void callMethod(HelloWorldEn hello) {
+//		hello.sayHello();
+//	}
+	
+	public static void callMethod(HelloWorldKo hello) {
+		hello.sayHello();
+	}
+
+}
