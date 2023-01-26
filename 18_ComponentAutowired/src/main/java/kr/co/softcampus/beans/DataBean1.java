c824041bd1a1355f17f34d50076d641ba43a170b
supra1nova <supra1nova@gmail.com>
2023-01-26 17:25:47 +0900

    component autowired

---
+++ 18_ComponentAutowired/src/main/java/kr/co/softcampus/beans/DataBean1.java
@@ -0,0 +1,8 @@
+package kr.co.softcampus.beans;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class DataBean1 {
+
+}
---
+++ 18_ComponentAutowired/src/main/java/kr/co/softcampus/beans/DataBean2.java
@@ -0,0 +1,8 @@
+package kr.co.softcampus.beans;
+
+import org.springframework.stereotype.Component;
+
+@Component("obj2")
+public class DataBean2 {
+
+}
